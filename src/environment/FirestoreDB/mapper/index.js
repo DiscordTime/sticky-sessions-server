2e381e495f82fddbd86e5071120eaa4891dcefed
Vinicius Albuquerque <viniciusalbuquerque@github.com>
2018-09-10 15:08:33 -0300

    Issue #27: Status code was not being returned when error occurred
    When the delete method was called and an error ocurred, the status
    code was still '200 OK'. Now it's '503 Service Unavailable'.
    
    Also, now IDs of DOCs are being returned.
    
    It fixes #27.

--- src/environment/FirestoreDB/mapper/index.js
+++ src/environment/FirestoreDB/mapper/index.js
@@ -2,6 +2,7 @@ module.exports.mapSnapshotToArray = function (snapshot, callback) {
   var array = []
   snapshot.forEach(doc => {
     var data = doc.data()
+    data['id'] = doc.id
     array.push(data)
   })
   callback(array)
