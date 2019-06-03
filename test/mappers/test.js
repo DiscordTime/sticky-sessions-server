615a53f2ebca7f6f2c68a10b1cfb3f847cfbed67
Vinicius Albuquerque <vinicius.aalbuquerque@gmail.com>
2019-06-03 07:02:31 -0300

    Adding tests to mappers

---
+++ test/mappers/test.js
@@ -0,0 +1,23 @@
+const chai = require('chai')
+const expect = chai.expect
+//const expect = chai.assert
+const describe = require('mocha').describe
+const it = require('mocha').it
+
+const ReqMock = require('./reqMock')
+const reqMock = new ReqMock()
+
+const NotesMapper = require('./../../src/router/notesRouter/mapper')
+const notesMapper = new NotesMapper()
+
+describe('Notes Router Mapper', function () {
+  describe('Validating AddNote Mapper', function () {
+    it('Should return a note', () => {
+      expect(() => notesMapper.mapAddNoteToDomain(reqMock.mockAddNoteCorrectReq())).to.not.throw() 
+    })
+
+    it('Should throw an Validation Error', () => {
+      expect(() => notesMapper.mapAddNoteToDomain(reqMock.mockAddNoteNotCorrectReq())).to.throw()
+    })
+  })
+})
