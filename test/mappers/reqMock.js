e3e14d5072763ec04a9e7ac045659a500d5e93a0
Vinicius Albuquerque <vinicius.aalbuquerque@gmail.com>
2019-06-03 07:02:31 -0300

    Adding tests to mappers

---
+++ test/mappers/reqMock.js
@@ -0,0 +1,25 @@
+class ReqMock {
+
+  mockAddNoteCorrectReq() {
+    return {
+      body: {
+	user: 'userTest',
+	description: 'test note',
+	session_id: 'abcz',
+	topic: 'start'
+      }
+    }
+  }
+
+  mockAddNoteNotCorrectReq() {
+    return {
+      body: {
+	user: 'userTest',
+	description: 'test note',
+	topic: 'start'
+      }
+    }
+  }
+}
+
+module.exports = ReqMock
